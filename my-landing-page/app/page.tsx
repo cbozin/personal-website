5fe82d13f1225068a59bf939bdb8baa63790f59b
cbozin <carolynbozhinov@gmail.com>
2025-07-09 17:37:27 -0700

    basics working

---
+++ my-landing-page/app/page.tsx
@@ -0,0 +1,13 @@
+import Features from "@/components/features-1";
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection/>
+      <Features/>
+      <FooterSection/>
+    </>
+  );
+}
