c95c958d63b886aeea7b14fab3fde84bdbc8c816
cbozin <carolynbozhinov@gmail.com>
2025-07-14 15:12:50 -0700

    fixed linting issue

--- my-landing-page/components/hero-section.tsx
+++ my-landing-page/components/hero-section.tsx
@@ -21,7 +21,7 @@ export default function HeroSection() {
                     <div className="pb-24 pt-12 md:pb-32 lg:pb-40 lg:pt-32"> 
                         <div className="mx-auto max-w-4xl px-6 text-center"> 
                             <h1 className="mt-8 text-balance text-5xl font-medium md:text-6xl xl:text-7xl">
-                                Hi, I'm Carolyn.
+                                Hi, I&apos;m Carolyn.
                             </h1>
                             <p className="mt-8 text-pretty text-lg mx-auto max-w-2xl"> 
                                 I build data-driven solutions—from full-stack apps to AI/ML pipelines—using Python, GCP, and modern web stacks.
